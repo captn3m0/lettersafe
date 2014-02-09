40bae2e1c5af6bd199b545bdaa206e1dc78b4831
Abhay Rana <capt.n3m0@gmail.com>
2014-02-09 06:09:54 +0530

    Saves outbound emails in db
    - Also redirects to /send after login

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ module.exports = function(db){
 			res.send("Email sent");
 		},
 		send: function(req, res){
+			if(req.session.username == undefined)
+				res.redirect('/login');
 			res.render('send', {title:"Compose Email", username:req.session.username})
 		},
 		sendpost: function(req, res){
@@ -26,6 +28,15 @@ module.exports = function(db){
 				from:req.body.from+"@lettersafe.in",
 				text:req.body.text,
 				subject:"Hello world"
+			})
+			db.collection('emails').insert({
+				user: req.session.username,
+	            timestamp: Date.now(),
+	            label: 'sent',
+	            msg: req.body.text
+			}, function(err, data){
+				if(err) throw err;
+				console.log(data);
 			})
 			res.send("Email sent");	
 		}
